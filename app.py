076f0a2ae0d58c8b73da473e4eb74e0b6816618e
donmaleek <bamboloshari@gmail.com>
2025-04-27 17:17:13 +0300

    Add tests and Render deployment config

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ def home():
     return "Hello, World from Eng.Mathias Flask App!"
 
 if __name__ == '__main__':
+    port = int (os.environ.get("port" ,5000))
     app.run(host='0.0.0.0', port=5000)
 
